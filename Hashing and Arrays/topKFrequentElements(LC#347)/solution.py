a96152d602ae9585b76877aa91ea8b72e53520e8
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2023-12-25 12:50:27 +0530

    Create solution.py

---
+++ Hashing and Arrays/topKFrequentElements(LC#347)/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            if n in count:
+                count[n]+=1
+            else:
+                count[n]=1
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
