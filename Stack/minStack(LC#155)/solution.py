5ced1c07c8c880109fa29e99867ce92184c010c1
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2023-12-28 13:14:36 +0530

    Create solution.py

---
+++ Stack/minStack(LC#155)/solution.py
@@ -0,0 +1,24 @@
+class MinStack:
+    
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val2=val
+        if self.minStack:
+            val2 = self.minStack[-1]
+        
+        val = min(val, val2)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
