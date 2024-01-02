15e8d00fbf47089fd97fe671c987ab7e8e5f0fb5
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-01-02 22:24:49 +0530

    Create solution.py

---
+++ Binary Search/binarySearch(LC#704)/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+            
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid = l + (r-l)//2
+            if target==nums[mid]:
+                return mid
+            if target< nums[mid]:
+                r=mid-1
+            else:
+                l=mid+1
+        return -1
