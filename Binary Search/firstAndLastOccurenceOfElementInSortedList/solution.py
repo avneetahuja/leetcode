f338406ae27a12986af56b3e992ecdb387b10bc7
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-01-05 01:08:40 +0530

    Update solution.py

--- Binary Search/firstAndLastOccurenceOfElementInSortedList/solution.py
+++ Binary Search/firstAndLastOccurenceOfElementInSortedList/solution.py
@@ -1,23 +1,23 @@
 class Solution:
   def firstAndLastOccurenceOfElementInSortedList(self,nums,target):
-    l,r,first,last=0,0,-1,-1
+    l,r,first,last=0,len(nums)-1,-1,-1
     while l<=r:
-      m=l+(r-l)//2
-      if nums[m]==target:
-        first=m
-        r=m-1
-      elif nums[m]<target:
-        l=m+1
-      else:
-        r=m-1
-    l,r=0,0
+        m=l+(r-l)//2
+        if nums[m]==target:
+            first=m
+            r=m-1
+        elif nums[m]<target:
+            l=m+1
+        else:
+            r=m-1
+    l,r=0,len(nums)-1
     while l<=r:
-      m=l+(r-l)//2
-      if nums[m]==target:
-        last=m
-        l=m+1
-      elif nums[m]<target:
-        l=m+1
-      else:
-        r=m-1
+        m=(l+r)//2
+        if nums[m]==target:
+            last=m
+            l=m+1
+        elif nums[m]<target:
+            l=m+1
+        else:
+            r=m-1
     return[first,last]
