02446922c3e77d40f820dbb5fed7ee87b69e8f7a
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-02-02 08:30:31 -0700

    Create solution.py

---
+++ Trees/InOrderTraversal(LC#94)/solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.res=[]
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.inOrder(root)
+        return self.res
+    def inOrder(self,root):
+        if not root:
+            return
+        self.inOrder(root.left)
+        self.res.append(root.val)
+        self.inOrder(root.right)
