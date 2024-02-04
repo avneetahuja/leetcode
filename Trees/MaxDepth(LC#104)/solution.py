54d28ffa6a7821bc7e3382d264c45efeb9df496b
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-02-04 11:26:47 -0700

    Create solution.py

---
+++ Trees/MaxDepth(LC#104)/solution.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self.maxD(root)
+    def maxD(self,node):
+        if not node:
+            return 0
+        l = self.maxD(node.left)
+        r = self.maxD(node.right)
+        return 1 + max(l,r)
