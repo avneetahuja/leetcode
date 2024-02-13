0504ad4f0ea21923e035f9a03e6f86abcb4af187
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-02-13 10:07:42 -0700

    Create solution.py

---
+++ Trees/constructFromInorderAndPreOrder(LC#105)/solution.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        rootIndex = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:rootIndex+1], inorder[:rootIndex])
+        root.right = self.buildTree(preorder[rootIndex+1:],inorder[rootIndex+1:])
+        return root
