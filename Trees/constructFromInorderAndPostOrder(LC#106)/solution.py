1cebbad00b3810e61619d377e8e7653ff089b396
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-02-13 10:15:55 -0700

    Create solution.py

---
+++ Trees/constructFromInorderAndPostOrder(LC#106)/solution.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not inorder or not postorder:
+            return None
+        root  = TreeNode(postorder[-1])
+        rootIndex = inorder.index(postorder[-1])
+        root.left = self.buildTree(inorder[:rootIndex],postorder[:rootIndex])
+        root.right = self.buildTree(inorder[rootIndex+1:],postorder[rootIndex:len(postorder)-1])
+        return root
