77f6ac91ea0cd5205e8c6d40af99c2be0a0809d2
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-02-20 08:54:58 -0700

    Create solution.py

---
+++ DP/Unbounded Knapsack/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def unboundedKnapsack(self,weights,values,capacity,size):
+        t = [[0 if i == 0 or j == 0 else -1 for j in range(capacity + 1)] for i in range(size + 1)]
+        for i in range(1,size+1):
+            for j in range(1,capacity+1):
+                if weights[i-1]<=j:
+                    t[i][j] = max(values[i-1]+t[i][j-weights[i-1]],t[i-1][j])#t[i] instead of t[i-1] since if we chose that element we can consider it again :)
+                else:
+                    t[i][j] = t[i-1][j]
+        print(t)
+        return t[size][capacity]
+    
+            
+s = Solution
+print(s.zeroOneTopDown(s,weights=[1,2,4,5],values=[1,4,5,7],capacity=7,size=4))
