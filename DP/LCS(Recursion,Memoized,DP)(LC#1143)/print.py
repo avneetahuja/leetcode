ea94f24a3be0c810be6326474e2f7fa631accfa0
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-02-23 10:24:33 -0700

    Create print.py

---
+++ DP/LCS(Recursion,Memoized,DP)(LC#1143)/print.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonSubsequence(self, x: str, y: str) -> int:
+        m,n = len(x),len(y)
+        t = [[0 for i in range(n+1)]for j in range(m+1)]
+        res = "" #If you want to print it
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if x[i-1] == y[j-1]:
+                    t[i][j] = 1+t[i-1][j-1]
+                    res +=x[i-1]
+                else:
+                    t[i][j] = max(t[i-1][j],t[i][j-1])
+        print(res)
+        return t[m][n]
