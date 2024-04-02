84915c3353e27bd879af0ebcf747716680046796
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-04-01 19:56:02 -0700

    Create solution.py

---
+++ Graphs/ Number of Connected Components in an Undirected Graph (LC#323)/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        root = [i for i in range(n)]
+        rank = [1] * n
+        c=n
+        def find(i):
+            if root[i]==i:
+                return i
+            root[i] = find(root[i])
+            return root[i]
+        def union(i,j):
+            nonlocal c
+            rI = find(i)
+            rJ = find(j)
+            if rI!=rJ:
+                if rank[rI] > rank[rJ]:
+                    root[rJ] = rI
+                elif rank[rJ]>rank[rI]:
+                    root[rI] = rJ
+                else:
+                    root[rJ] = rI
+                    rank[rI]+=1
+                c-=1
+        for edge in edges:
+            union(edge[0],edge[1])
+        return c
