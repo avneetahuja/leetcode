e5288a40aed0b4f8958c9017c9ef8ac2dafe46e7
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-04-19 14:26:17 -0700

    Update solution.py

--- Graphs/NumberOfIslands(LC#200)/solution.py
+++ Graphs/NumberOfIslands(LC#200)/solution.py
@@ -1,27 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = {}
         count = 0
+        dirs = [(-1,0),(0,-1),(1,0),(0,1)]
+        C = len(grid[0])
+        R = len(grid)
 
         def search(i,j):
-            if (i,j) in visited:
-                return
-            visited[(i,j)] = True
-            if j+1<len(grid[0]) and grid[i][j+1]=="1":
-                search(i,j+1)
-            if i+1<len(grid) and grid[i+1][j]=="1":
-                search(i+1,j)
-            if j-1>=0 and grid[i][j-1]=="1":
-                search(i,j-1)
-            if i-1>=0 and grid[i-1][j]=="1":
-                search(i-1,j)
-            return
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=="0" or (i,j) in visited:
-                    continue
-                else:
-                    count+=1
+            for dx,dy in dirs:
+                x = i+dx
+                y = j+dy
+                if 0<=x<R and 0<=y<C and grid[x][y]=="1":
+                    grid[x][y]='#'
+                    search(x,y)
+
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j]=="1":
+                    grid[i][j]='#'
                     search(i,j)
-        return count
+                    count+=1
         
+        return count
