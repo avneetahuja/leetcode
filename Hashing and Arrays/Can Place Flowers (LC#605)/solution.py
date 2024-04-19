2b3f2b6d896c1ce3d323b5a03b35918b2282619d
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2024-04-19 14:30:33 -0700

    Create solution.py

---
+++ Hashing and Arrays/Can Place Flowers (LC#605)/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], m: int) -> bool:
+        count = 0
+        n = len(flowerbed)
+        if n<=1:
+            if flowerbed[0] ==0:
+                return True
+            elif m<1:
+                return True
+        if flowerbed[0] == 0 and flowerbed[1]==0:
+            flowerbed[0]=1
+            count+=1
+        if flowerbed[-1]==0 and flowerbed[-2]==0:
+            flowerbed[-1]=1
+            count+=1
+        for i in range(2,n-1):
+            if flowerbed[i]==0 and flowerbed[i-1]==0 and flowerbed[i+1]==0:
+                flowerbed[i]=1
+                count+=1
+        return m<=count
