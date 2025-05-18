d113d8ed56b0325ef2c2f4c19ac8e21f337ab86b
Avneet Ahuja <82617800+avneetahuja@users.noreply.github.com>
2025-05-17 18:49:12 -0700

    Create solution.py

---
+++ Graphs/Course Schedule (LC#210)/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = {c:[] for c in range(numCourses)}
+        for c,p in prerequisites:
+            graph[c].append(p)
+        res = []
+        visited,cycle = set(),set()
+        def dfs(c):
+            
+            if c in cycle:
+                return False
+            if c in visited:
+                return True
+
+            cycle.add(c)
+            for pre in graph[c]:
+                if not dfs(pre):
+                    return False
+            cycle.remove(c)
+            visited.add(c)
+            res.append(c)
+            return True
+        for c in range(numCourses):
+            if not dfs(c):
+                return []
+        return res
